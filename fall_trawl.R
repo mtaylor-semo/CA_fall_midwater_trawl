f20aef4c1c3662e3c59e446c63645b6c8e77b3a2
Mike Taylor <mtaylor@semo.edu>
2019-10-23 10:48:29 -0500

    Reads files into dataframe, plots results.

--- fall_trawl.R
+++ fall_trawl.R
@@ -20,12 +20,13 @@ chunk_length = end_year - start_year + 3
 # Functions ---------------------------------------------------------------
 
 f = function(x, pos){
-  x[1] <- str_extract(x[1], species_names) # pattern match to extract spp names
-  x[1] <- str_replace_all(x[1], " ", "_")
+  sp_name <- str_extract(x[1], species_names) # pattern match to extract spp names
+  x[1] <- str_replace_all(sp_name, " ", "_")
   x[1] <- str_to_lower(x[1])
 #  filename <- paste(x[1], ".txt", sep="")
 #  write_lines(x, path = filename)
   tsv <- read_tsv(x, skip = 1)            # read as tab-separated
+  tsv <- mutate(tsv, species = sp_name)
   write_csv(tsv, paste0(x[1], ".csv"))    # write as csv
 }  
 
@@ -40,5 +41,30 @@ read_lines_chunked("ca_fall_trawl_data_raw.txt",
 
 
 # Read csv files ----------------------------------------------------------
+#
+# Convert to dataframe and arrange.
 
+# Based on this blog post by Claus Wilke
+# https://serialmentor.com/blog/2016/6/13/reading-and-combining-many-tidy-data-files-in-R
+# See the comments for map_dfr(), files <- dir(pattern = "*.csv")
+fishes_raw <- files %>% 
+  map_dfr(~ read_csv(.))
 
+fishes <- fishes_raw %>% 
+  gather(key = month, value = number, Sept:Dec) %>% 
+  select(species, Year, month, number)
+
+fishes %>% 
+#  filter(Total <= 10000) %>% 
+  ggplot(aes(x = Year, y = number)) +
+  geom_line() +
+  facet_wrap(vars(species)) +
+  geom_smooth(method = "lm")
+
+## Try without Longfin Smelt, which has really high abundance
+fishes_small <- fishes %>% 
+  filter(species == "Delta Smelt" |
+           species == "Splittail")
+
+fishes_small %>% ggplot(aes(x = Year, y = Total)) +
+  geom_line(aes(color = species))
