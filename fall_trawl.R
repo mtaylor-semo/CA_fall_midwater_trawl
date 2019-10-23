f0db3e38e07de04d6dda15447b2f2207f8af19b8
Mike Taylor <mtaylor@semo.edu>
2019-10-23 07:46:18 -0500

    csv files properly named with just species and underscores.

--- fall_trawl.R
+++ fall_trawl.R
@@ -6,17 +6,22 @@ library(stringr)
 start_year = 1967
 end_year <- year(Sys.Date())
 
+# For string extract
+species_names <- c("Striped Bass|Delta Smelt|Longfin Smelt|American Shad|Splittail|Threadfin Shad")
+
 # Add one line to get year range, 1 line for species name, and 1 line for column headers
 chunk_length = end_year - start_year + 3 
 
 f = function(x, pos){
-  filename = paste(x[1], ".txt", sep="")
-  write_lines(x, path = filename)
-  tsv <- read_tsv(file = filename, skip = 1)
+  x[1] <- str_extract(x[1], species_names)
+  x[1] <- str_replace_all(x[1], " ", "_")
+  x[1] <- str_to_lower(x[1])
+  #  x[1] <- str_to_lower(x[1])
+#  filename <- paste(x[1], ".txt", sep="")
+#  write_lines(x, path = filename)
+  tsv <- read_tsv(x, skip = 1)
   write_csv(tsv, paste0(x[1], ".csv"))
 }  
 read_lines_chunked("ca_fall_trawl_data_raw.txt",
                    SideEffectChunkCallback$new(f), 
                    chunk_size = chunk_length)
-
-read_tsv("chunk_1.txt", skip = 1)
